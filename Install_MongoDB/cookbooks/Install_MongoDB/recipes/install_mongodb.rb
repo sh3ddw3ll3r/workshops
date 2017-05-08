ee955e1959a9312dcf7ea0dab167059fe4714ffd
Eric Hunsberger <ehunsberger@deloitte.com>
2017-05-08 19:51:51 -0400

    updaed comments

--- Install_MongoDB/cookbooks/Install_MongoDB/recipes/install_mongodb.rb
+++ Install_MongoDB/cookbooks/Install_MongoDB/recipes/install_mongodb.rb
@@ -1,8 +1,13 @@
 
+# set variable for path to mongodb.repo file
 mongo_repo_file = '/etc/yum.repos.d/mongodb.repo'
 
+# build the mongodb.repo file from a template, which builds based on OS architecture (32bit vs 64 bit)
 template mongo_repo_file do
   source 'mongodb.repo.erb'
 end
 
-yum_package 'mongodb-org'
+# install mongodb
+yum_package 'mongodb-org' do
+  action :install
+end
--- Install_MongoDB/cookbooks/Install_MongoDB/recipes/start_mongodb.rb
+++ Install_MongoDB/cookbooks/Install_MongoDB/recipes/start_mongodb.rb
@@ -1,3 +1,4 @@
+# start mongosdb, and set teh service to start on server boot
 service "mongod" do
   action [:enable, :start]
 end
