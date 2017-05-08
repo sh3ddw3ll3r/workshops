13d2b1b3fad6d7510bcb0e74647c0b957946435d
Eric Hunsberger <ehunsberger@deloitte.com>
2017-05-08 19:45:21 -0400

    Added inspec check to verify mongodb package install

--- Install_MongoDB/cookbooks/Install_MongoDB/test/smoke/default/default_test.rb
+++ Install_MongoDB/cookbooks/Install_MongoDB/test/smoke/default/default_test.rb
@@ -5,6 +5,10 @@
 # The Inspec reference, with examples and extensive documentation, can be
 # found at http://inspec.io/docs/reference/resources/
 
+describe package('mongodb-org') do
+    it { should be_installed  }
+end
+
 describe processes('mongod') do
   its('users') { should eq ['mongod'] }
   its('list.length') {should eq 1}
